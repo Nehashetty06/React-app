01bdf22fa89ac67f578b09f3cc6d6c196959cae0
Neha <nehashettyn@gmail.com>
2023-08-05 11:38:04 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/* const header = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+      "div",
+      { id: "child1" },
+      React.createElement("h1", {}, "Child tag")
+    ),
+  ],
+  [
+    React.createElement(
+      "div",
+      { id: "child2" },
+      React.createElement("h1", {}, "Child tag")
+    ),
+  ]
+); */
+
+const header = React.createElement('h1',{},"First React App");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
